1e28f603a11b2e0cb0d394113e1e9a6e8e12f905
ltkai <dimitriluke@protonmail.com>
2022-11-03 22:04:30 +0700

    Tao trang them moi lien he

--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,21 @@ const routes = [
     name: "contactbook",
     component: ContactBook,
   },
-
   {
     path: "/:pathMatch(.*)*",
     name: "notfound",
     component: () => import("@/views/NotFound.vue"),
   },
-
   {
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/contacts/add",
+    name: "contact.add",
+    component: () => import("@/views/ContactAdd.vue"),
     props: true, // Truyền các biến trong $route.params vào làm props
   },
 ];
